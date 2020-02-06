9a706c812b5167e27e02caeaead69479fb4d595d
Patrick <patrick@247wallst.com>
2020-02-06 17:39:40 -0500

    Update wp local config
    Add VIP set up constants and debugging for local

--- conf/wp-local-config.php
+++ conf/wp-local-config.php
@@ -5,9 +5,20 @@
  * @package docker-wordpress-vip-go
  */
 
+ini_set( 'display_errors', 1 );
+set_error_handler(function() {
+	error_log(print_r(debug_backtrace(), true));
+	return true;
+}, E_USER_NOTICE);
+defined( 'WP_DEBUG' ) or define( 'WP_DEBUG', true );
+defined( 'WP_DEBUG_LOG' ) or define( 'WP_DEBUG_LOG', true );
 // Conditionally turn on HTTPS since we're behind nginx-proxy.
 if ( isset( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) && 'https' === $_SERVER['HTTP_X_FORWARDED_PROTO'] ) { // Input var ok.
 	$_SERVER['HTTPS'] = 'on';
+}
+
+if ( file_exists( __DIR__ . '/wp-content/vip-config/vip-config.php' ) ) {
+    require_once( __DIR__ . '/wp-content/vip-config/vip-config.php' );
 }
 
 define( 'UPLOADS', 'wp-content/images' );
@@ -17,6 +28,8 @@ define( 'VIP_GO_ENV', 'local' );
 
 // Disable automatic updates.
 define( 'AUTOMATIC_UPDATER_DISABLED', true );
+define( 'DISALLOW_FILE_EDIT', true );
+define( 'DISALLOW_FILE_MODS', true );
 
 // This provides the host and port of the development Memcached server. The host
 // should match the container name in `docker-compose.memcached.yml`. If you
