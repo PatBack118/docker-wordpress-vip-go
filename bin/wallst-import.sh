844fb28129e9a89dfa2e00b31b7ea4dc80626ea0
Patrick <patrick@247wallst.com>
2020-03-27 12:55:55 -0400

    Update import script to include redirects

--- bin/wallst-import.sh
+++ bin/wallst-import.sh
@@ -1,1 +1,1 @@
-cd /var/www/html && wp import /usr/local/247wallst/247wallst.201.xml --authors=create
+cd /var/www/html && wp import /usr/local/247wallst/247wallst.201.xml --authors=create && wp import /usr/local/247wallst/safe-redirects.xml
